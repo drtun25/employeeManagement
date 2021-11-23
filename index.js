bdd6d45f4062b3ebca10f735d4d4d656f4dce29a
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-11-23 01:43:13 -0500

    new stuff2

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ const connection = mysql.createConnection({
     database: "employee_db"
 });
 
+inquirer
+  .prompt({
+    type: "list",
+    message: "What do you want to do?",
+    name: "option",
+    choices: ["add", "view", "remove"]
+  })
+  .then(function(answer) {
+    console.log(answer);
+    if (answer.option === "add") {
+      inquirer
+        .prompt({
+          type: "list",
+          message: "What do you want to add?",
+          name: "option",
+          choices: ["department", "role", "employee"]
+        })
+        .then(function(answer) {
+            console.log(answer);
