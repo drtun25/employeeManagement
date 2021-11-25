76fc3a71096f6ce457a7edfa72a04dafbca1dc7e
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-11-24 21:27:37 -0500

    sql continued

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL ,
   name VARCHAR(45) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL ,
+  title VARCHAR(45) NULL,
+  salary DECIMAL(10) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL ,
+  first_name VARCHAR(45) NULL,
+  last_name VARCHAR(45) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
   PRIMARY KEY (id)
 );
