8034b19b1a498d38a7b287aa5053e887fb49dbf8
Adedotun O. Albert-Ojeikere <dotun1m@gmail.com>
2021-11-24 22:32:42 -0500

    questions

--- index.js
+++ index.js
@@ -22,4 +22,51 @@ connection.connect(function (err) {
   askQuestions();
 });
 
-//the app starts
+//the app starts with questions
+function askQuestions() {
+  inquirer.prompt({
+      message: "what would you like to do?",
+      type: "list",
+      choices: [
+          "view all employees",
+          "view all departments",
+          "add employee",
+          "add department",
+          "add role",
+          "update employee role",
+          "QUIT"
+      ],
+      name: "choice"
+  }).then(answers => {
+      console.log(answers.choice);
+      switch (answers.choice) {
+          case "view all employees":
+              viewEmployees()
+              break;
+
+          case "view all departments":
+              viewDepartments()
+              break;
+
+          case "add employee":
+              addEmployee()
+              break;
+
+          case "add department":
+              addDepartment()
+              break;
+
+          case "add role":
+              addRole()
+              break;
+
+          case "update employee role":
+              updateEmployeeRole();
+              break;
+
+          default:
+              connection.end()
+              break;
+      }
+  })
+}
